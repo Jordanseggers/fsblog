c5191da119c53095f9939514868b8f33ce48ca70
Jordan Eggers <jordaneggers@Jordans-MBP.lan>
2022-04-26 14:28:21 -0400

    finish 4.2

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const http = require('http')
 const app = require('./app')
-//const express = require('express')
-//const app = express()
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-//const cors = require('cors')
-//const mongoose = require('mongoose')
 
 const server = http.createServer(app)
 
